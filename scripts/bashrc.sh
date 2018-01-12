5fd13b9d149b0f344eb510feee67465619382909
Peter Weissig <peterweissig@github.com>
2018-01-12 02:53:21 +0100

    added bash commands for pololu

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -2,10 +2,11 @@
 # 2018 01 11
 
 export ROBOLIB_PATH_SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )/"
-export ROBOLIB_PATH="$(cd "${ROBOLIB_PATH_SCRIPTS}../" \
-  && pwd )/"
+export ROBOLIB_PATH="$(cd "${ROBOLIB_PATH_SCRIPTS}../" && pwd )/"
 
 export ROBOLIB_MAKEFILE="${ROBOLIB_PATH}make/Makefile"
+
+export POLOLU_PATH="$(cd "${ROBOLIB_PATH}../pololu" && pwd )/"
 
 
 #***************************[repository]**************************************
@@ -58,4 +59,40 @@ function robolib_data() {
 
     make data -f ${ROBOLIB_PATH}make/Makefile
 }
+
+
+#***************************[pololu]******************************************
+# 2018 01 11
+
+function pololu_repo_update() {
+
+    if [ -d "${POLOLU_PATH}" ]; then
+        cd ${POLOLU_PATH}
+        git pull
+    else
+        echo "path \"${POLOLU_PATH}\" not found"
+    fi
+}
+
+function pololu_repo_status() {
+
+    if [ -d "${POLOLU_PATH}" ]; then
+        cd ${POLOLU_PATH}
+        git status --untracked
+    else
+        echo "path \"${POLOLU_PATH}\" not found"
+    fi
+}
+
+function pololu_repo_make() {
+
+    if [ -d "${POLOLU_PATH}" ]; then
+        cd ${POLOLU_PATH}
+        make clean
+        make
+    else
+        echo "path \"${POLOLU_PATH}\" not found"
+    fi
+}
+
 
